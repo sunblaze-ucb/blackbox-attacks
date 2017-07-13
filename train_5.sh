d19793cd47440900a6fe769982ca22d29b6b3157
w <none>
2017-07-14 04:59:45 +0800

    wide variant

---
+++ train_5.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export CUDA_VISIBLE_DEVICES=4
+python resnet_main_adv_wide.py --train_data_path=cifar-10-batches-bin/data_batch* --log_root=log_adv_wide --train_dir=log_adv_wide/train --dataset=cifar10 --num_gpus=1
