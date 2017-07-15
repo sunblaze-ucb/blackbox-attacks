04589d26d40c30229b0fc61d6a8e8f6440b973eb
arjunbhagoji <arjunbhagoji@gmail.com>
2017-07-14 20:07:02 -0700

    reorganize, add gnuplot ready output

--- query_based_attack.py
+++ query_based_attack.py
@@ -21,16 +21,132 @@ BATCH_EVAL_NUM = 100
 CLIP_MIN = 0
 CLIP_MAX = 1
 
-def white_box_fgsm(prediction, target_model, x, logits, y, X_test, targets, targets_cat, dim):
+def wb_img_save(adv_pred_np, targets, eps, X_adv_t):
+    img_count = 0
+    for k in range(30):
+        adv_label_wb = np.argmax(adv_pred_np[k].reshape(1, FLAGS.NUM_CLASSES),1)
+        if adv_label_wb[0] != targets[k]:
+            img.imsave( 'images/'+args.method+'/'+args.norm+'/{}_wb_{}_{}_{}.png'.format(target_model_name,
+                adv_label_wb, targets[k], eps),
+                X_adv_t[k].reshape(FLAGS.IMAGE_ROWS, FLAGS.IMAGE_COLS)*255, cmap='gray')
+            img_count += 1
+        if img_count >=10:
+            return
+    return
+
+def est_img_save(i, adv_prediction, curr_target, eps, x_adv):
+    img_count = 0
+    if i==0:
+        for k in range(30):
+            adv_label = np.argmax(adv_prediction[k].reshape(1, FLAGS.NUM_CLASSES),1)
+            if adv_label[0] != curr_target[k]:
+                img.imsave( 'images/'+args.method+'/'+args.norm+'/{}_{}_{}_{}_{}.png'.format(target_model_name,
+                    adv_label, curr_target[k], eps, args.delta),
+                    x_adv[k].reshape(FLAGS.IMAGE_ROWS, FLAGS.IMAGE_COLS)*255, cmap='gray')
+                img_count += 1
+            if img_count >= 10:
+                return
+    return
+
+def wb_write_out(eps, white_box_error, wb_norm):
+    if RANDOM is False:
+        ofile = open('output_data/'+args.method+'_'+args.norm+'_based_classwise'+str(eps)+'_'+str(target_model_name)+'.txt', 'a')
+        ofile.write('{}'.format(white_box_error))
+        print('Fraction of targets achieved (white-box) for {}: {}'.format(target, white_box_error))
+    else:
+        ofile = open('output_data/wb_'+target_model_name+'_'+args.norm+'_attack.txt','a')
+        ofile.write('{} {} {} \n'.format(eps, white_box_error, wb_norm))
+        print('Fraction of targets achieved (white-box): {}'.format(white_box_error))
+
+    return
+
+def est_write_out(eps, success, avg_l2_perturb):
+    if RANDOM is False:
+        ofile = open('output_data/'+args.method+'_'+args.norm+'_classwise'+str(eps)+'_'+str(target_model_name)+'.txt', 'a')
+        ofile.write(' {} \n'.format(success))
+        print('Fraction of targets achieved (query-based) with {} for {}: {}'.format(target_model_name, target, success))
+    else:
+        ofile = open('output_data/'+args.method+'_'+args.norm+'_'+target_model_name+'_attack.txt','a')
+        ofile.write('{} {} {} {}\n'.format(args.delta, eps, success, avg_l2_perturb))
+        print('Fraction of targets achieved (query-based): {}'.format(success))
+    ofile.close()
+    return
+
+def estimated_grad_attack(X_test, x, targets, prediction, eps, dim):
+    success = 0
+    avg_l2_perturb = 0
+    time1 = time.time()
+    for i in range(BATCH_EVAL_NUM):
+        if i % 10 ==0:
+            print('{}, {}'.format(i, eps))
+        curr_sample = X_test[i*BATCH_SIZE:(i+1)*BATCH_SIZE].reshape((BATCH_SIZE, FLAGS.IMAGE_ROWS, FLAGS.IMAGE_COLS, 1))
+
+        curr_target = targets[i*BATCH_SIZE:(i+1)*BATCH_SIZE]
+
+        curr_prediction = K.get_session().run([prediction], feed_dict={x: curr_sample, K.learning_phase(): 0})[0]
+
+        p_t = curr_prediction[np.arange(BATCH_SIZE), list(curr_target)]
+
+        if 'query_based' in args.method:
+            grad_est = finite_diff_method(prediction, x, curr_sample, curr_target, dim)
+        elif 'one_shot' in args.method:
+            grad_est = one_shot_method(prediction, x, curr_sample, curr_target)
+        # Getting gradient of the loss
+        loss_grad = -1.0 * grad_est/p_t[:, None, None, None]
+        # Getting signed gradient of loss
+        if args.norm == 'linf':
+            normed_loss_grad = np.sign(loss_grad)
+        elif args.norm == 'l2':
+            grad_norm = np.linalg.norm(loss_grad.reshape(BATCH_SIZE, dim), axis = 1)
+            indices = np.where(grad_norm != 0.0)
+            normed_loss_grad = np.zeros_like(curr_sample)
+            normed_loss_grad[indices] = loss_grad[indices]/grad_norm[indices, None, None, None]
+
+        if '_un' in args.method:
+            x_adv = np.clip(curr_sample + eps * normed_loss_grad, 0, 1)
+        else:
+            x_adv = np.clip(curr_sample - eps * normed_loss_grad, 0, 1)
+
+        # Getting the norm of the perturbation
+        perturb_norm = np.linalg.norm((x_adv-curr_sample).reshape(BATCH_SIZE, dim), axis=1)
+        perturb_norm_batch = np.mean(perturb_norm)
+        avg_l2_perturb += perturb_norm_batch
+
+        adv_prediction = K.get_session().run([prediction], feed_dict={x: x_adv, K.learning_phase(): 0})[0]
+        success += np.sum(np.argmax(adv_prediction,1) == curr_target)
+
+        est_img_save(i, adv_prediction, curr_target, eps, x_adv)
+
+
+    success = 100.0 * float(success)/(BATCH_SIZE*BATCH_EVAL_NUM)
+
+    if '_un' in args.method:
+        success = 100.0 - success
+
+    avg_l2_perturb = avg_l2_perturb/BATCH_EVAL_NUM
+
+    est_write_out(eps, success, avg_l2_perturb)
+
+    time2 = time.time()
+    print('Average l2 perturbation: {}'.format(avg_l2_perturb))
+    print('Total time: {}, Average time: {}'.format(time2-time1, (time2 - time1)/(BATCH_SIZE*BATCH_EVAL_NUM)))
+
+    return
+
+
+def white_box_fgsm(prediction, target_model, x, logits, y, X_test, targets, targets_cat, eps, dim):
 
     #Get gradient from model
     grad = gen_grad(x, logits, y)
 
-    # signed gradient
-    normed_grad = K.sign(grad)
+    # normalized gradient
+    if args.norm == 'linf':
+        normed_grad = K.sign(grad)
+    elif args.norm == 'l2':
+        normed_grad = K.l2_normalize(grad, axis = (1,2,3))
 
     # Multiply by constant epsilon
-    scaled_grad = args.eps * normed_grad
+    scaled_grad = eps * normed_grad
 
     # Add perturbation to original example to obtain adversarial example
     if '_un' in args.method:
@@ -46,29 +162,16 @@ def white_box_fgsm(prediction, target_model, x, logits, y, X_test, targets, targ
 
     adv_pred_np = K.get_session().run([prediction], feed_dict={x: X_adv_t, K.learning_phase(): 0})[0]
 
-    print(adv_pred_np.shape)
-
-    for k in range(1):
-        adv_label_wb = np.argmax(adv_pred_np[k].reshape(1, FLAGS.NUM_CLASSES),1)
-        img.imsave( 'images/'+args.method+'/{}_wb_{}_{}_{}.png'.format(target_model_name,
-                adv_label_wb, targets[k], args.eps),
-                X_adv_t[k].reshape(FLAGS.IMAGE_ROWS, FLAGS.IMAGE_COLS)*255, cmap='gray')
+    wb_img_save(adv_pred_np, targets, eps, X_adv_t)
 
     white_box_error = tf_test_error_rate(target_model, x, X_adv_t, targets_cat)
     if '_un' not in args.method:
         white_box_error = 100.0 - white_box_error
 
-    if RANDOM is False:
-        ofile = open('output_data/'+args.method+'_based_classwise'+str(args.eps)+'_'+str(target_model_name)+'.txt', 'a')
-        ofile.write('{}'.format(white_box_error))
-        print('Fraction of targets achieved (white-box) for {}: {}'.format(target, white_box_error))
-    else:
-        ofile = open('output_data/'+args.method+'_attack.txt','a')
-        ofile.write('White-box, {}, {} : {} \n'.format(target_model_name, args.eps, white_box_error))
-        print('Fraction of targets achieved (white-box): {}'.format(white_box_error))
-
     wb_norm = np.mean(np.linalg.norm((X_adv_t-X_test_slice).reshape(BATCH_SIZE*BATCH_EVAL_NUM, dim), axis=1))
     print('Average white-box l2 perturbation: {}'.format(wb_norm))
+
+    wb_write_out(eps, white_box_error, wb_norm)
 
     return
 
@@ -112,7 +215,6 @@ def one_shot_method(prediction, x, curr_sample, curr_target):
     return grad_est
 
 
-
 def main(target_model_name, target=None):
     np.random.seed(0)
     tf.set_random_seed(0)
@@ -147,69 +249,19 @@ def main(target_model_name, target=None):
         targets = np.random.randint(10, size = BATCH_SIZE*BATCH_EVAL_NUM)
     targets_cat = np_utils.to_categorical(targets, FLAGS.NUM_CLASSES).astype(np.float32)
 
-    white_box_fgsm(prediction, target_model, x, logits, y, X_test, targets, targets_cat, dim)
+    if args.norm == 'linf':
+        eps_list = list(np.linspace(0.0, 0.1, 5))
+        eps_list.extend(np.linspace(0.2, 0.5, 7))
+        # eps_list = [0.3]
+    elif args.norm == 'l2':
+        eps_list = list(np.linspace(0.0, 9.0, 28))
+        # eps_list = [5.0]
 
-    success = 0
-    avg_l2_perturb = 0
-    time1 = time.time()
-    for i in range(BATCH_EVAL_NUM):
-        print('{}'.format(i))
-        curr_sample = X_test[i*BATCH_SIZE:(i+1)*BATCH_SIZE].reshape((BATCH_SIZE, FLAGS.IMAGE_ROWS, FLAGS.IMAGE_COLS, 1))
-        if i ==0:
-            for k in range(1):
-                img.imsave('images/mnist_{}.png'.format(k),
-                    curr_sample[k].reshape(FLAGS.IMAGE_ROWS, FLAGS.IMAGE_COLS) *255, cmap='gray')
-        curr_target = targets[i*BATCH_SIZE:(i+1)*BATCH_SIZE]
+    for eps in eps_list:
+        white_box_fgsm(prediction, target_model, x, logits, y, X_test, targets,
+                        targets_cat, eps, dim)
 
-        curr_prediction = K.get_session().run([prediction], feed_dict={x: curr_sample, K.learning_phase(): 0})[0]
-
-        p_t = curr_prediction[np.arange(BATCH_SIZE), list(curr_target)]
-
-        if 'query_based' in args.method:
-            grad_est = finite_diff_method(prediction, x, curr_sample, curr_target, dim)
-        elif 'one_shot' in args.method:
-            grad_est = one_shot_method(prediction, x, curr_sample, curr_target)
-        # Getting gradient of the loss
-        loss_grad = -1.0 * grad_est/p_t[:, None, None, None]
-        # Getting signed gradient of loss
-        normed_loss_grad = np.sign(loss_grad)
-
-        if '_un' in args.method:
-            x_adv = np.clip(curr_sample + args.eps * normed_loss_grad, 0, 1)
-        else:
-            x_adv = np.clip(curr_sample - args.eps * normed_loss_grad, 0, 1)
-
-        # Getting the norm of the perturbation
-        perturb_norm = np.linalg.norm((x_adv-curr_sample).reshape(BATCH_SIZE, dim), axis=1)
-        perturb_norm_batch = np.mean(perturb_norm)
-        print('{}'.format(perturb_norm_batch))
-        avg_l2_perturb += perturb_norm_batch
-
-        adv_prediction = K.get_session().run([prediction], feed_dict={x: x_adv, K.learning_phase(): 0})[0]
-        success += np.sum(np.argmax(adv_prediction,1) == curr_target)
-        if i==0:
-            for k in range(1):
-                adv_label = np.argmax(adv_prediction[k].reshape(1, FLAGS.NUM_CLASSES),1)
-                img.imsave( 'images/'+args.method+'/{}_{}_{}_{}_{}.png'.format(target_model_name,
-                        adv_label, curr_target[k], args.eps, args.delta),
-                        x_adv[k].reshape(FLAGS.IMAGE_ROWS, FLAGS.IMAGE_COLS) *255, cmap='gray')
-    success = 100.0 * float(success)/(BATCH_SIZE*BATCH_EVAL_NUM)
-
-    if '_un' in args.method:
-        success = 100.0 - success
-
-    if RANDOM is False:
-        ofile = open('output_data/'+args.method+'_classwise'+str(args.eps)+'_'+str(target_model_name)+'.txt', 'a')
-        ofile.write(' {} \n'.format(success))
-        print('Fraction of targets achieved (query-based) with {} for {}: {}'.format(target_model_name, target, success))
-    else:
-        ofile = open('output_data/'+args.method+'_attack.txt','a')
-        ofile.write('Query based {}, {}, {} : {} \n'.format(args.delta, target_model_name, args.eps, success))
-        print('Fraction of targets achieved with {}: {}'.format(target_model_name, success))
-    time2 = time.time()
-    print('Average l2 perturbation: {}'.format(avg_l2_perturb))
-    print('Total time: {}, Average time: {}'.format(time2-time1, (time2 - time1)/(BATCH_SIZE*BATCH_EVAL_NUM)))
-    ofile.close()
+        estimated_grad_attack(X_test, x, targets, prediction, eps, dim)
 
 if __name__ == "__main__":
     import argparse
@@ -217,11 +269,9 @@ if __name__ == "__main__":
     parser.add_argument("target_model", help="target model for attack")
     parser.add_argument("--method", choices=['query_based', 'one_shot',
                         'query_based_un', 'one_shot_un'], default='query_based')
-    parser.add_argument("--eps", type=float, default=0.3,
-                        help="FGS attack scale")
     parser.add_argument("--delta", type=float, default=0.01,
                         help="local perturbation")
-    parser.add_argument("--norm", type=str, default='inf',
+    parser.add_argument("--norm", type=str, default='linf',
                             help="Norm to use for attack")
     args = parser.parse_args()
 
@@ -233,7 +283,7 @@ if __name__ == "__main__":
         RANDOM = True
 
     if RANDOM is False:
-        ofile = open('output_data/'+args.method+'_classwise'+str(args.eps)+
+        ofile = open('output_data/'+args.method+'_classwise'+str(eps)+
                         '_'+str(target_model_name)+'.txt', 'a')
         ofile.write('{} \n'.format(args.delta))
         for i in range(FLAGS.NUM_CLASSES):
