8a37aa954c0036ecbb7a55a3da66fd1213be699c
arjunbhagoji <arjunbhagoji@gmail.com>
2017-07-17 20:38:24 -0700

    added cw loss

--- query_based_attack.py
+++ query_based_attack.py
@@ -17,7 +17,7 @@ FLAGS = flags.FLAGS
 
 RANDOM = True
 BATCH_SIZE = 100
-BATCH_EVAL_NUM = 1
+BATCH_EVAL_NUM = 100
 CLIP_MIN = 0
 CLIP_MAX = 1
 
@@ -26,7 +26,7 @@ def wb_img_save(adv_pred_np, targets, eps, X_adv_t):
     for k in range(30):
         adv_label_wb = np.argmax(adv_pred_np[k].reshape(1, FLAGS.NUM_CLASSES),1)
         if adv_label_wb[0] != targets[k]:
-            img.imsave( 'images/'+args.method+'/'+args.norm+'/{}_wb_{}_{}_{}.png'.format(target_model_name,
+            img.imsave( 'images/wb/'+args.norm+'/'+args.loss_type+'{}_{}_{}_{}.png'.format(target_model_name,
                 adv_label_wb, targets[k], eps),
                 X_adv_t[k].reshape(FLAGS.IMAGE_ROWS, FLAGS.IMAGE_COLS)*255, cmap='gray')
             img_count += 1
@@ -40,8 +40,9 @@ def est_img_save(i, adv_prediction, curr_target, eps, x_adv):
         for k in range(30):
             adv_label = np.argmax(adv_prediction[k].reshape(1, FLAGS.NUM_CLASSES),1)
             if adv_label[0] != curr_target[k]:
-                img.imsave( 'images/'+args.method+'/'+args.norm+'/{}_{}_{}_{}_{}.png'.format(target_model_name,
-                    adv_label, curr_target[k], eps, args.delta),
+                img.imsave( 'images/'+args.method+'/'+args.norm+'/'+args.loss_type+
+                                '_{}_{}_{}_{}_{}.png'.format(target_model_name,
+                                adv_label, curr_target[k], eps, args.delta),
                     x_adv[k].reshape(FLAGS.IMAGE_ROWS, FLAGS.IMAGE_COLS)*255, cmap='gray')
                 img_count += 1
             if img_count >= 10:
@@ -50,23 +51,25 @@ def est_img_save(i, adv_prediction, curr_target, eps, x_adv):
 
 def wb_write_out(eps, white_box_error, wb_norm):
     if RANDOM is False:
-        ofile = open('output_data/'+args.method+'_'+args.norm+'_based_classwise'+str(eps)+'_'+str(target_model_name)+'.txt', 'a')
+        ofile = open('output_data/wb_'+args.loss_type+'_'+args.norm+'_based_classwise'+str(eps)+'_'+str(target_model_name)+'.txt', 'a')
         ofile.write('{}'.format(white_box_error))
         print('Fraction of targets achieved (white-box) for {}: {}'.format(target, white_box_error))
     else:
-        ofile = open('output_data/wb_'+target_model_name+'_'+args.norm+'_attack.txt','a')
+        if '_un' in args.method:
+            ofile = open('output_data/wb_un_'+args.loss_type+'_'+args.norm+'_'+target_model_name+'.txt','a')
+        else:
+            ofile = open('output_data/wb_'+args.loss_type+'_'+args.norm+'_'+target_model_name+'.txt','a')
         ofile.write('{} {} {} \n'.format(eps, white_box_error, wb_norm))
         print('Fraction of targets achieved (white-box): {}'.format(white_box_error))
-
     return
 
 def est_write_out(eps, success, avg_l2_perturb):
     if RANDOM is False:
-        ofile = open('output_data/'+args.method+'_'+args.norm+'_classwise'+str(eps)+'_'+str(target_model_name)+'.txt', 'a')
+        ofile = open('output_data/'+args.method+'_'+args.loss_type+'_'+args.norm+'_classwise'+str(eps)+'_'+str(target_model_name)+'.txt', 'a')
         ofile.write(' {} \n'.format(success))
         print('Fraction of targets achieved (query-based) with {} for {}: {}'.format(target_model_name, target, success))
     else:
-        ofile = open('output_data/'+args.method+'_'+args.norm+'_'+target_model_name+'_attack.txt','a')
+        ofile = open('output_data/'+args.method+'_'+args.loss_type+'_'+args.norm+'_'+target_model_name+'.txt','a')
         ofile.write('{} {} {} {}\n'.format(args.delta, eps, success, avg_l2_perturb))
         print('Fraction of targets achieved (query-based): {}'.format(success))
     ofile.close()
@@ -83,8 +86,31 @@ def xent_est(prediction, x, x_plus_i, x_minus_i, curr_target):
 
     return single_grad_est
 
+def CW_est(logits, x, x_plus_i, x_minus_i, curr_sample, curr_target):
+    curr_logits = K.get_session().run([logits], feed_dict={x: curr_sample,
+                                                    K.learning_phase(): 0})[0]
+    curr_logits[np.arange(BATCH_SIZE), list(curr_target)] = -1e4
+    max_indices = np.argmax(curr_logits, 1)
+    logit_plus = K.get_session().run([logits], feed_dict={x: x_plus_i,
+                                                K.learning_phase(): 0})[0]
+    logit_plus_t = logit_plus[np.arange(BATCH_SIZE), list(curr_target)]
+    logit_plus_max = logit_plus[np.arange(BATCH_SIZE), list(max_indices)]
+
+    logit_minus = K.get_session().run([logits], feed_dict={x: x_minus_i,
+                                                K.learning_phase(): 0})[0]
+    logit_minus_t = logit_minus[np.arange(BATCH_SIZE), list(curr_target)]
+    logit_minus_max = logit_minus[np.arange(BATCH_SIZE), list(max_indices)]
+
+    logit_t_grad_est = (logit_plus_t - logit_minus_t)/args.delta
+    logit_max_grad_est = (logit_plus_max - logit_minus_max)/args.delta
+
+    return logit_t_grad_est, logit_max_grad_est
+
+
 def finite_diff_method(prediction, logits, x, curr_sample, curr_target, p_t, dim):
     grad_est = np.zeros((BATCH_SIZE, FLAGS.IMAGE_ROWS, FLAGS.IMAGE_COLS, FLAGS.NUM_CHANNELS))
+    logits_np = K.get_session().run([logits], feed_dict={x: curr_sample,
+                                                    K.learning_phase(): 0})[0]
     for j in range(dim):
         basis_vec = np.zeros((BATCH_SIZE, FLAGS.IMAGE_ROWS, FLAGS.IMAGE_COLS, FLAGS.NUM_CHANNELS))
         row = int(j/FLAGS.IMAGE_COLS)
@@ -92,13 +118,31 @@ def finite_diff_method(prediction, logits, x, curr_sample, curr_target, p_t, dim
         basis_vec[:, row, col] = 1.
         x_plus_i = np.clip(curr_sample + args.delta * basis_vec, CLIP_MIN, CLIP_MAX)
         x_minus_i = np.clip(curr_sample - args.delta * basis_vec, CLIP_MIN, CLIP_MAX)
-        if args.CW_loss == 0:
+        if args.loss_type == 'cw':
+            logit_t_grad_est, logit_max_grad_est = CW_est(logits, x, x_plus_i,
+                                            x_minus_i, curr_sample, curr_target)
+            if '_un' in args.method:
+                single_grad_est = logit_t_grad_est - logit_max_grad_est
+            else:
+                single_grad_est = logit_max_grad_est - logit_t_grad_est
+        elif args.loss_type == 'xent':
             single_grad_est = xent_est(prediction, x, x_plus_i, x_minus_i, curr_target)
         grad_est[:, row, col] = single_grad_est.reshape((BATCH_SIZE,1))
-
     # Getting gradient of the loss
-    if args.CW_loss == 0:
+    if args.loss_type == 'xent':
         loss_grad = -1.0 * grad_est/p_t[:, None, None, None]
+    elif args.loss_type == 'cw':
+        logits_np_t = logits_np[np.arange(BATCH_SIZE), list(curr_target)].reshape(BATCH_SIZE)
+        logits_np[np.arange(BATCH_SIZE), list(curr_target)] = -1e4
+        max_indices = np.argmax(logits_np, 1)
+        logits_np_max = logits_np[np.arange(BATCH_SIZE), list(max_indices)].reshape(BATCH_SIZE)
+        logit_diff = logits_np_t - logits_np_max
+        if '_un' in args.method:
+            zero_indices = np.where(logit_diff + args.conf < 0.0)
+        else:
+            zero_indices = np.where(-logit_diff + args.conf < 0.0)
+        grad_est[zero_indices[0]] = np.zeros((len(zero_indices), FLAGS.IMAGE_ROWS, FLAGS.IMAGE_COLS, FLAGS.NUM_CHANNELS))
+        loss_grad = grad_est
 
     return loss_grad
 
@@ -156,9 +200,12 @@ def estimated_grad_attack(X_test, x, targets, prediction, logits, eps, dim):
             normed_loss_grad = np.zeros_like(curr_sample)
             normed_loss_grad[indices] = loss_grad[indices]/grad_norm[indices, None, None, None]
 
-        if '_un' in args.method:
-            x_adv = np.clip(curr_sample + eps * normed_loss_grad, 0, 1)
-        else:
+        if args.loss_type == 'xent':
+            if '_un' in args.method:
+                x_adv = np.clip(curr_sample + eps * normed_loss_grad, 0, 1)
+            else:
+                x_adv = np.clip(curr_sample - eps * normed_loss_grad, 0, 1)
+        elif args.loss_type == 'cw':
             x_adv = np.clip(curr_sample - eps * normed_loss_grad, 0, 1)
 
         # Getting the norm of the perturbation
@@ -170,7 +217,6 @@ def estimated_grad_attack(X_test, x, targets, prediction, logits, eps, dim):
         success += np.sum(np.argmax(adv_prediction,1) == curr_target)
 
         est_img_save(i, adv_prediction, curr_target, eps, x_adv)
-
 
     success = 100.0 * float(success)/(BATCH_SIZE*BATCH_EVAL_NUM)
 
@@ -191,7 +237,16 @@ def estimated_grad_attack(X_test, x, targets, prediction, logits, eps, dim):
 def white_box_fgsm(prediction, target_model, x, logits, y, X_test, targets, targets_cat, eps, dim):
 
     #Get gradient from model
-    grad = gen_grad(x, logits, y)
+    if args.loss_type == 'xent':
+        grad = gen_grad(x, logits, y)
+    elif args.loss_type == 'cw':
+        real = tf.reduce_sum(targets_cat*logits, 1)
+        other = tf.reduce_max((targets_cat)*logits - (targets_cat*10000), 1)
+        if '_un' in args.method:
+            loss = tf.maximum(0.0,real-other+args.conf)
+        else:
+            loss = tf.maximum(0.0,other-real+args.conf)
+        grad = K.gradients(loss, [x])[0]
 
     # normalized gradient
     if args.norm == 'linf':
@@ -203,16 +258,20 @@ def white_box_fgsm(prediction, target_model, x, logits, y, X_test, targets, targ
     scaled_grad = eps * normed_grad
 
     # Add perturbation to original example to obtain adversarial example
-    if '_un' in args.method:
-        adv_x_t = K.stop_gradient(x + scaled_grad)
-    else:
+    if args.loss_type == 'xent':
+        if '_un' in args.method:
+            adv_x_t = K.stop_gradient(x + scaled_grad)
+        else:
+            adv_x_t = K.stop_gradient(x - scaled_grad)
+    elif args.loss_type == 'cw':
         adv_x_t = K.stop_gradient(x - scaled_grad)
 
     adv_x_t = K.clip(adv_x_t, CLIP_MIN, CLIP_MAX)
 
     X_test_slice = X_test[:BATCH_SIZE*BATCH_EVAL_NUM]
 
-    X_adv_t = batch_eval([x, y], [adv_x_t], [X_test_slice, targets_cat])[0]
+    X_adv_t = K.get_session().run([adv_x_t], feed_dict={x: X_test_slice, y: targets_cat ,K.learning_phase(): 0})[0]
+    # batch_eval([x, y], [adv_x_t], [X_test_slice, targets_cat])[0]
 
     adv_pred_np = K.get_session().run([prediction], feed_dict={x: X_adv_t, K.learning_phase(): 0})[0]
 
@@ -264,9 +323,9 @@ def main(target_model_name, target=None):
     targets_cat = np_utils.to_categorical(targets, FLAGS.NUM_CLASSES).astype(np.float32)
 
     if args.norm == 'linf':
-        # eps_list = list(np.linspace(0.0, 0.1, 5))
-        # eps_list.extend(np.linspace(0.2, 0.5, 7))
-        eps_list = [0.3]
+        eps_list = list(np.linspace(0.0, 0.1, 5))
+        eps_list.extend(np.linspace(0.2, 0.5, 7))
+        # eps_list = [0.3]
     elif args.norm == 'l2':
         eps_list = list(np.linspace(0.0, 9.0, 28))
         # eps_list = [5.0]
@@ -287,8 +346,10 @@ if __name__ == "__main__":
                         help="local perturbation")
     parser.add_argument("--norm", type=str, default='linf',
                             help="Norm to use for attack")
-    parser.add_argument("--CW_loss", type=int, default=0,
-                            help="Using CW loss instead of xent")
+    parser.add_argument("--loss_type", type=str, default='xent',
+                            help="Choosing which type of loss to use")
+    parser.add_argument("--conf", type=float, default=0.0,
+                                help="Strength of CW sample")
     args = parser.parse_args()
 
     target_model_name = basename(args.target_model)
