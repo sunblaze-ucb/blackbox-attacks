6657e44970e142b7b7ea0001346862ee4f8c47d6
arjunbhagoji <arjunbhagoji@gmail.com>
2017-08-03 20:46:48 -0700

    bash for multiple run

---
+++ group_run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -ex
+
+export CUDA_VISIBLE_DEVICES=1
+
+python query_based_attack.py models/modelA --loss_type=cw --group_size=7
+python query_based_attack.py models/modelA --loss_type=cw --group_size=35
+python query_based_attack.py models/modelA --loss_type=cw --group_size=70
+python query_based_attack.py models/modelA --loss_type=cw --group_size=140
+python query_based_attack.py models/modelA --loss_type=cw --group_size=350
+
+python query_based_attack.py models/modelA_adv --loss_type=cw --group_size=7
+python query_based_attack.py models/modelA_adv --loss_type=cw --group_size=35
+python query_based_attack.py models/modelA_adv --loss_type=cw --group_size=70
+python query_based_attack.py models/modelA_adv --loss_type=cw --group_size=140
+python query_based_attack.py models/modelA_adv --loss_type=cw --group_size=350
